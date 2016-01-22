e94bda11c4e9fc6c6b1dc0a51b2457017b2ec785
Facundo Estevez <fmestevez@gmail.com>
2016-01-22 15:55:25 -0300

    Facu - Big ass switch refactor

--- astyanax-cql/src/main/java/com/netflix/astyanax/cql/util/DataTypeMapping.java
+++ astyanax-cql/src/main/java/com/netflix/astyanax/cql/util/DataTypeMapping.java
@@ -1,58 +1,78 @@
 package com.netflix.astyanax.cql.util;
 
 import com.datastax.driver.core.DataType;
+import com.datastax.driver.core.LocalDate;
 import com.datastax.driver.core.Row;
+
+import java.math.BigDecimal;
+import java.net.InetAddress;
+import java.nio.ByteBuffer;
+import java.util.*;
 
 public class DataTypeMapping {
 
+	// TODO implement complex types
 	public static <T> Object getDynamicColumn(Row row, String columnName, DataType dataType) {
-		
+
 		switch(dataType.getName()) {
 
-		case ASCII:
-		    return row.getString(columnName);
-		case BIGINT:
-		    return row.getLong(columnName);
-		case BLOB:
-		    return row.getBytes(columnName);
-		case BOOLEAN:
-		    return row.getBool(columnName);
-		case COUNTER:
-		    return row.getLong(columnName);
-		case DECIMAL:
-		    return row.getDecimal(columnName);
-		case DOUBLE:
-		    return row.getDouble(columnName);
-		case FLOAT:
-		    return row.getFloat(columnName);
-		case INET:
-		    return row.getInet(columnName);
-		case INT:
-		    return row.getInt(columnName);
-		case TEXT:
-		    return row.getString(columnName);
-		case TIMESTAMP:
-		    return row.getDate(columnName);
-		case UUID:
-		    return row.getUUID(columnName);
-		case VARCHAR:
-		    return row.getString(columnName);
-		case VARINT:
-		    return row.getLong(columnName);
-		case TIMEUUID:
-		    return row.getUUID(columnName);
 		case LIST:
 		    throw new UnsupportedOperationException("Collection objects not supported for column: " + columnName);
 		case SET:
 		    throw new UnsupportedOperationException("Collection objects not supported for column: " + columnName);
 		case MAP:
-			return row.getMap(columnName, Object.class, Object.class);
-		    //throw new UnsupportedOperationException("Collection objects not supported for column: " + columnName);
-		case CUSTOM:
-		    throw new UnsupportedOperationException("Collection objects not supported for column: " + columnName);
-		    
+			List<DataType> types = dataType.getTypeArguments();
+			return row.getMap(columnName, getTypeClass(types.get(0)), getTypeClass(types.get(1)));
+
 		default:
-		    throw new UnsupportedOperationException("Unrecognized object for column: " + columnName);
+			return row.get(columnName, getTypeClass(dataType));
+		}
+	}
+
+	private static Class<?> getTypeClass(DataType dataType) {
+		switch(dataType.getName()) {
+
+			case ASCII:
+				return String.class;
+			case BIGINT:
+				return Long.class;
+			case BLOB:
+				return ByteBuffer.class;
+			case BOOLEAN:
+				return Boolean.class;
+			case COUNTER:
+				return Long.class;
+			case DECIMAL:
+				return BigDecimal.class;
+			case DOUBLE:
+				return Double.class;
+			case FLOAT:
+				return Float.class;
+			case INET:
+				return InetAddress.class;
+			case INT:
+				return Integer.class;
+			case TEXT:
+				return String.class;
+			case TIMESTAMP:
+				return LocalDate.class;
+			case UUID:
+				return UUID.class;
+			case VARCHAR:
+				return String.class;
+			case VARINT:
+				return Long.class;
+			case TIMEUUID:
+				return UUID.class;
+			case LIST:
+				return List.class;
+			case SET:
+				return Set.class;
+			case MAP:
+				return Map.class;
+
+			default:
+				throw new UnsupportedOperationException("Unrecognized object for DataType: " + dataType.getName());
 		}
 	}
 
